9144c575e72ac2bd858b7caa3f2693ef89e2df32
DaveSholy <48659370+DaveSholy@users.noreply.github.com>
2019-05-04 01:00:06 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
- client.user.setGame("Twitch", "https://www.twitch.tv/Alpha");
+ client.user.setGame("التأخر في الانتقام يجعل الضربة أشد قساوة", "https://www.twitch.tv/Alpha");
 });
  
  
