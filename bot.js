932a84eebf57362cca39cca02c13f41d5506a35f
DaveSholy <48659370+DaveSholy@users.noreply.github.com>
2019-05-08 21:25:34 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
- client.user.setGame("Stay away from me now.", "https://www.twitch.tv/Alpha");
+ client.user.setGame("▷ ◉──────── 00:40.", "https://www.twitch.tv/Alpha");
 });
  
 
